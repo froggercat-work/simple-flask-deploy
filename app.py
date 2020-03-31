9512acbe47a00f720775f30448cc5d021941ac99
froggercat <froggercat@gmail.com>
2020-03-31 16:20:57 -0400

    app.py setup complete

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
+import pandas as pd
+from sqlalchemy import create_engine
+from config import dbusr, dbpwd
 
 app = Flask(__name__)
+
+engine = create_engine(f"postgresql://{dbusr}:{dbpwd}@localhost/test")
 
 @app.route("/")
 def index():
@@ -8,7 +13,7 @@ def index():
 
 @app.route("/psqltest")
 def psqltest():
-    return "NOT IMPLEMENTED"
+    return Response(pd.read_sql("SELECT * FROM actor LIMIT 10", engine).to_json(orient="records"), mimetype="application/json")
 
 if __name__ == "__main__":
     app.run()
