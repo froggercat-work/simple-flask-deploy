a5a25d8e2ac157e28e75f4ea530b953ff1c85bea
Sondra Miller <sondra.n.miller@gmail.com>
2020-10-03 13:23:46 -0400

    changed init_db to read config.py connection string

--- init_db.py
+++ init_db.py
@@ -1,4 +1,5 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from config import cxnstring
 
-pd.read_csv("actors.csv").to_sql(name="actors", con=create_engine("sqlite:///my_db.sqlite"))
+pd.read_csv("actors.csv").to_sql(name="actors", con=create_engine(cxnstring), index=False) #create_engine("sqlite:///my_db.sqlite"))
